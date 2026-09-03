f507055c1c5609757266b18a8b0d9fa7cceb09bf
Leon Schäfer <schaefer_leon@mailbox.org>
2026-09-03 07:26:25 +0200

    use lastname in rive lineup

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -3,6 +3,7 @@ import type { SquadPlayer } from '@/api/kickbase';
 import { colors, positionColors, radius, spacing, statusColors, typography } from '@/theme/tokens';
 import { formatPoints } from '@/utils/format';
 import { StatusBadge } from './StatusBadge';
+import React from 'react';
 
 interface PlayerCardProps {
   player: SquadPlayer;
